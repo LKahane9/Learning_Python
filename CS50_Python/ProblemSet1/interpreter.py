926d669a481c58d4fdad90a734f7e4ddf1b930fe
LKahane9 <lkahane05@gmail.com>
2026-08-25 15:06:03 +1000

    finished interpreter.py

---
+++ CS50_Python/ProblemSet1/interpreter.py
@@ -0,0 +1,25 @@
+#ask person to input the math required, calculates and outputs as a float to one decimal
+# format as x y z, x is an int, y is either + - / or *, z is another int
+
+def main():
+    x, y, z = input("What equation do you want solved? ").strip().split(" ")
+    x = int(x)
+    z = int(z)
+    print(f"{x}, {y}, {z}.")
+    if y == "+":
+        answer = float(x + z)
+        print(round(answer , 1))
+    elif y == "-":
+        answer = float(x - z)
+        print(round(answer , 1))
+    elif y == "/":
+        answer = float(x / z)
+        print(round(answer , 1))
+    elif y == "*":
+        answer = float(x * z)
+        print(round(answer , 1))
+    else:
+        print("try a new operator (* + - /)")
+
+
+main()
