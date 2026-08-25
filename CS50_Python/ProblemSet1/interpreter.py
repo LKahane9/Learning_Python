d93baca706f50490f14b798a6481d3020b17409e
LKahane9 <lkahane05@gmail.com>
2026-08-25 15:12:32 +1000

    fixed it + removed line that i forgot previously

--- CS50_Python/ProblemSet1/interpreter.py
+++ CS50_Python/ProblemSet1/interpreter.py
@@ -1,11 +1,9 @@
 #ask person to input the math required, calculates and outputs as a float to one decimal
-# format as x y z, x is an int, y is either + - / or *, z is another int
-
+#format as x y z, x is an int, y is either + - / or *, z is another int
 def main():
     x, y, z = input("What equation do you want solved? ").strip().split(" ")
     x = int(x)
     z = int(z)
-    print(f"{x}, {y}, {z}.")
     if y == "+":
         answer = float(x + z)
         print(round(answer , 1))
@@ -13,13 +11,15 @@ def main():
         answer = float(x - z)
         print(round(answer , 1))
     elif y == "/":
-        answer = float(x / z)
-        print(round(answer , 1))
+        if z != 0:
+            answer = float(x / z)
+            print(round(answer , 1))
+        else:
+            print("You can't divide by zero.")
     elif y == "*":
         answer = float(x * z)
         print(round(answer , 1))
     else:
         print("try a new operator (* + - /)")
-
 
 main()
